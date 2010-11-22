7accde7c31f4dfd19a20edecfa17c7eb7c8432da
Magnus Woldrich <trapd00r@trapd00r.se>
2010-11-22 06:30:35 +0100

    7.05: fix

--- 7.05-english_number_words.pl
+++ 7.05-english_number_words.pl
@@ -1,12 +1,14 @@
 #!/usr/bin/perl
 use strict;
 
-=english number words - 7.05
+=description
+
 On financial documents, like cheques, numbers must sometimes be written
 in full words.
 Example: 175 must be written as one-seven-five.
 Write a predicate full_words/1 to print (non-negative) integer numbers in
 full words.
+
 =cut
 
 
@@ -25,12 +27,12 @@ my %map = (
 
 my $num = shift // 1024;
 
-my(@mapped) = split(//, $num);
+my @mapped = split(//, $num);
 
-my @result;
+my @result = ();
 for(@mapped) {
   push(@result, $map{$_});
 }
 
 my $eng = join('-', @result);
-print $eng, "\n";
+print "$eng\n";
